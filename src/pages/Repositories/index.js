debf43127e9c437cbc9317dbfefa55829aec8ecd
sansonvinicius <sansonvinicius@gmail.com>
2021-07-13 21:10:27 -0300

    criando link com react-outer-dom com botão voltar

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import * as S from './styled';
 
+
 export default function Repositories(){
     const[ repositories, setRepositories ] = useState([]);
     useEffect(()=>{
         let repositoriesName = localStorage.getItem('repositoriesName');
         repositoriesName = JSON.parse(repositoriesName);
         setRepositories(repositoriesName);
-        localStorage.clear();
+        //localStorage.clear();
     },[]);
     return(
         <S.Container>
@@ -19,6 +20,7 @@ export default function Repositories(){
                    )
                })}
             </S.List>
+            <S.LinkHome to="/">Voltar</S.LinkHome>
         </S.Container>
     )   
 }
